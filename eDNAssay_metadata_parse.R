19eaae6ab063312fccf619d0eeaa5328fa07a29e
John Kronenberger <101842362+forestJAK@users.noreply.github.com>
2022-03-20 17:47:58 -0600

    eDNAssay metadata parse

---
+++ eDNAssay_metadata_parse.R
@@ -0,0 +1,26 @@
+##################################################################################################
+### Script creates a metadata file from an alignment, formatted for eDNAssay
+##################################################################################################
+library(Biostrings)
+
+setwd()
+
+target_taxon <- "Target"
+
+fasta <- readDNAStringSet(file.choose(), format = "fasta") # FASTA file
+
+names <- fasta@ranges@NAMES
+
+extract_name <- function(x){
+  print(gsub("^[A-Z]+[^\\s]+\\s+([A-Z]+\\w+\\s+\\w+)\\s+.+", replacement = "\\1", x))
+}
+
+Taxon <- extract_name(names)
+Taxon[1:3] <- rep(target_taxon, 3)
+
+Name <- names
+
+Type <- c(rep("Oligo", 3), rep("Template", length(names) - 3))
+
+metadata <- data.frame(Taxon = Taxon, Name = Name, Type = Type)
+write.csv(metadata, "Mtadata.csv", row.names = FALSE)
