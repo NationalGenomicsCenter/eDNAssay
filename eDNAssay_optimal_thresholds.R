715b2fd289c81feae1b873cf6b236329fa0bd705
John Kronenberger <101842362+forestJAK@users.noreply.github.com>
2022-03-20 17:48:27 -0600

    eDNAssay optimal thresholds

---
+++ eDNAssay_optimal_thresholds.R
@@ -0,0 +1,32 @@
+##################################################################################################
+### Script calculates optimal thresholds for the model underlying eDNAssay
+##################################################################################################
+library(caret)
+setwd()
+load("TaqMan_rf_model.RData")
+
+thresh <-
+  thresholder(trainmodel_taqman, threshold = seq(0, 1, by = 0.01))
+colnames(thresh)[5] <- "PPV"
+colnames(thresh)[6] <- "NPV"
+thresh$PPV[is.nan(thresh$PPV)] <- 1
+thresh$FNrate <- 1 - thresh$NPV
+thresh$FPrate <- 1 - thresh$PPV
+
+cost <- function(FNrate, FPrate, ratio) {
+  print(FNrate * ratio + FPrate * 1)
+}
+
+min_cost <- function(FNrate, FPrate, ratio) {
+  print(which(cost(thresh$FNrate, thresh$FPrate, ratio) ==
+                min(
+                  cost(thresh$FNrate, thresh$FPrate, ratio)
+                )) / 100)
+}
+
+opt_threshold <- c()
+for (i in 1:100) {
+  opt_threshold[i] <- min_cost(thresh$FNrate, thresh$FPrate, i)
+}
+
+save(opt_threshold, file = "eDNAssay_optimal_thresholds.RData")
