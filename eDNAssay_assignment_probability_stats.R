15a974e0b7adbd038c5283c2071c52f9aba1075d
John Kronenberger <101842362+forestJAK@users.noreply.github.com>
2022-03-20 18:44:27 -0600

    eDNAssay assignment probability stats

--- eDNAssay_assignment_probability_stats.R
+++ eDNAssay_assignment_probability_stats.R
@@ -5,15 +5,21 @@ library(data.table)
 setwd()
 
 ### Assignment probability stats
-aps <- read.csv(file.choose(), header = TRUE) # Specificity file output by eDNAssay
+aps <-
+  read.csv(file.choose(), header = TRUE) # Specificity file output by eDNAssay
 aps <- data.table(aps)
 
-aps[, ':='(Min_amp = min(Amp), Max_amp = max(Amp), Mean_amp = mean(Amp), SD_amp = sd(Amp), N = length(Amp)), by = Taxon]
+aps[, ':='(
+  Min_amp = min(Amp),
+  Max_amp = max(Amp),
+  Mean_amp = mean(Amp),
+  SD_amp = sd(Amp),
+  N = length(Amp)
+), by = Taxon]
 aps[is.na(aps)] <- 0
-aps <- aps[order(Taxon), ]
+aps <- aps[order(Taxon),]
 aps <- unique(aps, by = "Taxon")
-aps <- aps[, -c(3:4)]
+aps <- aps[,-c(3:4)]
 
 write.csv(aps, "Assay_stats.csv", row.names = FALSE)
 print("Finished!")
-
