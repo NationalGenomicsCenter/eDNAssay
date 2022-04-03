d29a6f40af1462527c4561e25de4bdd619610b8b
John Kronenberger <101842362+forestJAK@users.noreply.github.com>
2022-04-03 14:51:28 -0600

    Update SYBR_model_training.R

--- SYBR_model_training.R
+++ SYBR_model_training.R
@@ -7,90 +7,24 @@ library(randomForest)
 library(beepr)
 library(plotROC)
 
+setwd("C:/Users/jkronenberger/Box/ESTCP/Assays/WORKING")
+
 ### Select input CSV file containing training data
-traindata <-
+traindata_all <-
   read.csv(file.choose(), header = TRUE) # Training data
 
-##################################################################################################
-### Define variables of interest and create training dataframe
-Assay <- as.factor(traindata$Assay)
-Species <- as.factor(traindata$Species)
-FRmm_total <- as.numeric(traindata$FRmm_total)
-FRmm_diff <- as.numeric(traindata$FRmm_diff) # Normalized difference
-FRmm_3p <-
-  as.numeric(paste(traindata$FRmm_3p / FRmm_total)) # Proportion
-FRmm_term <-
-  as.numeric(paste(traindata$FRmm_term / FRmm_total)) #Proportion
-FRmm_AA <-
-  as.numeric(paste(traindata$FRmm_AA / FRmm_total)) # Proportion
-FRmm_AG <-
-  as.numeric(paste(traindata$FRmm_AG / FRmm_total)) # Proportion
-FRmm_AC <-
-  as.numeric(paste(traindata$FRmm_AC / FRmm_total)) # Proportion
-FRmm_TT <-
-  as.numeric(paste(traindata$FRmm_TT / FRmm_total)) # Proportion
-FRmm_TG <-
-  as.numeric(paste(traindata$FRmm_TG / FRmm_total)) # Proportion
-FRmm_TC <-
-  as.numeric(paste(traindata$FRmm_TC / FRmm_total)) # Proportion
-FRmm_GG <-
-  as.numeric(paste(traindata$FRmm_GG / FRmm_total)) # Proportion
-FRmm_CC <-
-  as.numeric(paste(traindata$FRmm_CC / FRmm_total)) # Proportion
-FR_length <-
-  as.numeric(paste((traindata$F_length + traindata$R_length) / 2)) # Mean
-FR_Tm <-
-  as.numeric(paste((traindata$F_Tm + traindata$R_Tm) / 2)) # Mean
-FR_Tmdiff <-
-  as.numeric(paste(abs(traindata$F_Tm - traindata$R_Tm))) # Raw difference
-FR_GC <-
-  as.numeric(paste((traindata$F_GC + traindata$R_GC) / 2)) # Mean
-SYBR_results <- as.factor(traindata$SYBR_results)
-SYBR_keep <- as.factor(traindata$SYBR_keep)
-
+### Select variables of interest
 traindata <-
-  data.frame(
-    Assay,
-    Species,
-    FRmm_total,
-    FRmm_diff,
-    FRmm_3p,
-    FRmm_term,
-    FRmm_AA,
-    FRmm_AG,
-    FRmm_AC,
-    FRmm_TT,
-    FRmm_TG,
-    FRmm_TC,
-    FRmm_GG,
-    FRmm_CC,
-    FR_length,
-    FR_Tm,
-    FR_Tmdiff,
-    FR_GC,
-    SYBR_results,
-    SYBR_keep
-  )
-
-is.nan.data.frame <- function(x)
-  do.call(cbind, lapply(x, is.nan))
-traindata[is.nan(traindata)] <- 0
-
-traindata <- subset.data.frame(traindata, SYBR_keep == TRUE)
-traindata <- subset.data.frame(traindata, select = c(3:19))
+  subset.data.frame(traindata_all, select = c(4, 5, 7:19, 22))
 
 ##################################################################################################
 ### Pre-process SYBR data prior to cross-validation
 ### Assess response variable distribution
-table(traindata$SYBR_results) # Binary outcomes are roughly equal
+table(traindata$Results) # Binary outcomes are roughly equal
 
 ### Assess highly correlated variables
-varcor <- cor(traindata[,-17])
-findCorrelation(varcor, cutoff = 0.75) # Length strongly correlated with GC content
-
-### Remove GC content
-traindata <-
-  subset.data.frame(traindata, select = -16)
+varcor <- cor(traindata[,-16])
+findCorrelation(varcor, cutoff = 0.75) # No highly correlated variables
 
 ### Assess linearly dependent variables
 findLinearCombos(traindata[,-16]) # No linearly dependent predictors
@@ -112,7 +46,7 @@ tunegrid <- expand.grid(.mtry = c(1:15))
 set.seed(1111)
 trainmodel_sybr <-
   train(
-    SYBR_results ~ .,
+    Results ~ .,
     data = traindata,
     method = "rf",
     num.trees = 1000,
@@ -130,6 +64,8 @@ print(trainmodel_sybr)
 
 # trainresults_sybr <- predict(trainmodel_sybr, type="prob") # To output assignment probabilities
 trainresults_sybr <- predict(trainmodel_sybr)
+
+traindata[, 16] <- as.factor(traindata[, 16])
 traincm_sybr <-
   confusionMatrix(trainresults_sybr, traindata[, 16])
 print(traincm_sybr)
