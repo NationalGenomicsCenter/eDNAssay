5071f153c0e2aa45b73d4a3db2e54c40c78eac8f
John Kronenberger <101842362+forestJAK@users.noreply.github.com>
2022-04-05 16:03:33 -0600

    Update eDNAssay_metadata_parse.R

--- eDNAssay_metadata_parse.R
+++ eDNAssay_metadata_parse.R
@@ -5,11 +5,14 @@ library(Biostrings)
 library(stringr)
 
 alignment <- readDNAStringSet(file.choose(), format = "fasta") # FASTA file
-name <- alignment@ranges@NAMES
-name <- gsub("NC ", "NC", name)
 
-taxon <- word(name, start = 2, end=3, sep = fixed(" "))
+taxon <- alignment@ranges@NAMES
+taxon <- gsub("NC ", "NC", taxon)
+taxon <- gsub("UNVERIFIED: ", "", taxon)
+taxon <- word(taxon, start = 2, end=3, sep = fixed(" "))
 taxon[1:3] <- rep("Target", 3)
+
+name <- alignment@ranges@NAMES
 
 type <- c(rep("Oligo", 3), rep("Template", length(name) - 3))
 
